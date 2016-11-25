c1d6600f4413d55716d0bce6c53b96cbef3a0023
jordanpurinton <jordanomac@gmail.com>
2016-11-25 11:17:30 -0700

    add new route for a message

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
+// middleware
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // public folder holds static files to be accessible
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,14 @@
+// Sent from server to client
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('index'); // refers to index.hbs file
+});
+
+router.get('/message', function (req, res, next) {
+    res.render('node', {message: 'Hello!'});
 });
 
 module.exports = router;
