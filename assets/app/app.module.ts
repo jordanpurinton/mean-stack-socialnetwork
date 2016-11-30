61e23382fd32da2842a9425eceef18bcf30b992b
jordanpurinton <jordanomac@gmail.com>
2016-11-30 12:17:27 -0700

    fix component typo

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -7,7 +7,7 @@ import {MessageListComponent} from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
 import {MessagesComponent} from "./messages/messages.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
-import {HeaderComopnent} from "./header.component";
+import {HeaderComponent} from "./header.component";
 import {routing} from './app.routing';
 
 @NgModule({ // decorator defines what I'm going to use in addition to the class AppModule
@@ -18,7 +18,7 @@ import {routing} from './app.routing';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComopnent
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent] // set root component
--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -14,6 +14,6 @@ import {Component} from '@angular/core';
     `
 })
 
-export class HeaderComopnent{
+export class HeaderComponent{
 
 }
