0518928073d58a6264cb616e91d61f2f564ce93a
jordanpurinton <jordanomac@gmail.com>
2016-11-30 11:11:15 -0700

    set up initial app routes and page redirect

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,15 +5,22 @@ import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./messages/message.component";
 import {MessageListComponent} from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComopnent} from "./header.component";
+import {routing} from './app.routing';
 
 @NgModule({ // decorator defines what I'm going to use in addition to the class AppModule
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComopnent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent] // set root component
 })
 export class AppModule {
