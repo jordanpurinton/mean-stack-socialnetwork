175191f2caae63bd353cd749815680a296377db6
jordanpurinton <jordanomac@gmail.com>
2016-11-30 11:16:22 -0700

    add routing files

---
+++ assets/app/app.routing.ts
@@ -0,0 +1,13 @@
+import {Routes, RouterModule} from "@angular/router"
+import {MessagesComponent} from './messages/messages.component'
+import {AuthenticationComponent} from './auth/authentication.component'
+
+
+const APP_ROUTES: Routes = [
+    {path: '', redirectTo: '/messages', pathMatch: 'full'}, // this route will always redirect to /messages
+                                                            // set pathMatch to full so it ONLY reacts to '' if it is the full path
+    {path: 'messages', component: MessagesComponent},
+    {path: 'auth', component: AuthenticationComponent}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES); // this gets imported in our app.module
