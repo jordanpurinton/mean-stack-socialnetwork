09f1ed86a1522e5b9a5b0e822c8661d12f14696e
jordanpurinton <jordanomac@gmail.com>
2016-12-01 11:35:16 -0700

    set up messages route for saving message

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,6 @@
 // Sent from server to client
-
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
       res.render('index');
