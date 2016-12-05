87e9b477c4c66a9584b788439d4519cfc787117c
jordanpurinton <jordanomac@gmail.com>
2016-12-05 09:53:00 -0700

    modify routes to handle message updates

--- routes/messages.js
+++ routes/messages.js
@@ -36,4 +36,34 @@ router.post('/', function (req, res, next) { // this remains as '/' because we o
     });
 });
 
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'Message not found!',
+                error: {message: 'Message not found!'}
+            });
+        }
+        message.content = req.body.content;
+        message.save(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'Message not found!',
+                    error: err
+                });
+            }
+            res.status(200).json({ // if no err, return messages
+                message: 'Updated message',
+                obj: result
+            });
+        });
+    });
+});
+
 module.exports = router;
