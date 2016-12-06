ce83f0d66f42cfbaaf4b6ec6d94fc9c532865c21
jordanpurinton <jordanomac@gmail.com>
2016-12-06 08:09:06 -0700

    cosmetic changes

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -4,7 +4,7 @@ import {Component} from "@angular/core";
     selector: 'app-authentication',
     template:
         `<header class="row spacing">
-            <nav class="col-md-8 col-md-offset-2">
+            <nav class="col-md-8">
                 <ul class="nav nav-tabs">
                     <li routerLinkActive="active"><a [routerLink]="['signup']">Sign Up</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['signin']">Sign In</a></li>
--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -3,13 +3,12 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-header',
     template: `
+        <h4 style="opacity: .5; float: right;">A MEAN messenger<br><h6>by Jordan Purinton</h6></h4>
         <header class="row">
-            <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-pills">
                     <li routerLinkActive="active"><a [routerLink]="['/messages']">Messenger</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['/auth']">Authentication</a></li>
                 </ul>
-            </nav>
         </header>
     `
 })
