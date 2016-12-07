caa44677410c8cdb034a750f87678e9034b898a2
jordanpurinton <jordanomac@gmail.com>
2016-12-07 10:57:43 -0700

    add path for user sign in functionality w/ JWT

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var secret = require('bcryptjs');
 var User = require('../models/user');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function (req, res, next) { // TODO integrate SSL
     var user = new User ({
@@ -22,6 +23,35 @@ router.post('/', function (req, res, next) { // TODO integrate SSL
             obj: result
         })
 
+    });
+});
+
+router.post('/signin', function(req, res, next) {
+    User.findOne({email: req.body.email}, function(err, user){
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        if (!user) { // user not found
+            return res.status(401).json({
+                title: 'Email or password were incorrect',
+                error: {message: 'Email or password were incorrect'}
+            })
+        }
+        if (!secret.compareSync(req.body.password, user.password)) { // password is not the same
+            return res.status(401).json({
+                title: 'Email or password were incorrect',
+                error: {message: 'Email or password were incorrect'}
+            });
+        }
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200}); // 7200 sec or 2 hours
+        res.status(200).json({
+            message: 'Login successful',
+            token: token,
+            userId: user._id // will use later in front end for display purposes
+        });
     });
 });
 
