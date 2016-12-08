7695ccfbbabc1c14a6c317106ae19c71c8c8414e
jordanpurinton <jordanomac@gmail.com>
2016-12-08 08:57:05 -0700

    disable message posting unless user is logged in

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Message = require('../models/message');
+var jwt = require('jsonwebtoken');
 
 router.get('/', function (req, res, next) { // route '/' is /messages in this context
     Message.find()
@@ -16,6 +17,18 @@ router.get('/', function (req, res, next) { // route '/' is /messages in this co
                 obj: messages
             });
         });
+});
+
+router.use('/', function (req, res, next) { // will be used on every request except .get('/')
+    jwt.verify(req.query.token, 'secret', function(err, decoded){ // gives access to all query params, checks for token in params
+        if (err) {
+            return res.status(401).json({ // not authorized
+                title: 'Authentication failed',
+                error: err
+            });
+        }
+        next(); // let request continue if no err
+    });
 });
 
 router.post('/', function (req, res, next) { // this remains as '/' because we only go here if it begins with /messages
@@ -66,7 +79,7 @@ router.patch('/:id', function (req, res, next) {
     });
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
     Message.findById(req.params.id, function (err, message) {
         if (err) {
             return res.status(500).json({
